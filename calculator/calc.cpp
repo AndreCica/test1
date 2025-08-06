c8daa872b172d07a854387d4eb1ef3f3088b31a0
Andre Cicalese <cicaandre@yahoo.com>
2025-08-06 15:47:37 +0100

    starting to involve numbers

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -3,12 +3,15 @@
 
 
 int main(){
-    std::cout << "What is your name?" << std::endl;
+    std::cout << "Give me two numbers" << std::endl;
 
-    std::string input{};
-    std::cin >> input;
+    std::string input1{}, input2{};
+    std::cin >> input1;
+    std::cin >> input2;
 
-    std::cout << input << ", you are the youngest person ever" << std::endl;
+    std::cout << input1 << " and " << input2 << " are your two numbers" << std::endl;
+
+
 
     return 0;
 }
