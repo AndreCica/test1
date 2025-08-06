9a65cef210c4e1d57e2ca89dc8ac6bcd950b3e1e
Andre Cicalese <cicaandre@yahoo.com>
2025-08-06 17:22:48 +0100

    got a basic calculator working

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 
 int main(){
     std::cout << "Give me two numbers" << std::endl;
 
-    std::string input1{}, input2{};
+
+    float input1{}, input2{};
+    std::string mather{};
     std::cin >> input1;
     std::cin >> input2;
 
     std::cout << input1 << " and " << input2 << " are your two numbers" << std::endl;
 
+    std::cout << "Now give me an operator" << std::endl;
 
+    std::cin >> mather;
+    float solution;
+
+    if (mather == "+"){
+        solution = input1 + input2;
+    }
+    else if(mather == "-"){
+        solution = input1 - input2;
+    }
+    else if(mather == "/"){
+        solution = input1 / input2;
+    }
+    else if(mather == "*"){
+        solution = input1 * input2;
+    }  
+    else{
+        std::cout << "please put in an actual operator mate" << std::endl;
+    }
+
+    std::cout << "Your answer is " << solution << std::endl;
 
     return 0;
 }
